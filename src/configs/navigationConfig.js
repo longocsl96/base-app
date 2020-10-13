8cc0216e47177006969d11617f5a408ba05c6b1c
Lo Ngoc <mrboyvip.sl96@gmail.com>
2020-10-13 22:48:04 +0700

    Update base app

--- src/configs/navigationConfig.js
+++ src/configs/navigationConfig.js
@@ -5,12 +5,14 @@ import AppId from './appId'
 const navigationConfig = [
   {
     id: AppId.HOME,
+    type: 'item',
     title: 'menu.home',
     icon: <Icon.Home size={20} />,
     navLink: '/'
   },
   {
     id: AppId.USER,
+    type: 'item',
     title: 'menu.user',
     icon: <Icon.Mail size={20} />,
     permissions: ['admin', 'editor'],
@@ -21,6 +23,7 @@ const navigationConfig = [
 const getNativgationConfig = (appId) => {
   return navigationConfig.map((item) => {
     item.type = item.id === appId ? 'item' : 'external-link'
+    return item;
   })
 }
 
